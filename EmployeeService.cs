2d646e558d9e347826e82df2b5d2397414ce4ab6
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-23 18:08:56 +0100

    Just adding something anyway

--- EmployeeService.cs
+++ EmployeeService.cs
@@ -80,7 +80,7 @@ namespace ConsoleAdoDotNet
         {
             Console.WriteLine("\nAll Employees");
 
-            ConsoleTable table = new("Name", "Email", "Department", "Hire Date", "Salary");
+            ConsoleTable table = new("Name", "Email", "Department", "Hire Date", "Salary")
 
             using (MySqlConnection connection = new(_connectionString))
             {
